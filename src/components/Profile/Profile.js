222f46946ea07354df806c20cbf8099cbd577bc6
richard wetterauer <rhwette2022@gmail.com>
2023-04-12 14:01:11 -0500

    initial commit

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Profile.css';
+import SideBar from './SideBar/SideBar';
+import './SideBar/SideBar.css';
+import ClothesSection from './ClothesSection/ClothesSection';
+import './ClothesSection/ClothesSection.css';
+
+const Profile = ({
+  nameOfPerson,
+  cards,
+  onCardClick,
+  onCardDelete,
+  onAddNewClick,
+  clothingType,
+  clickHandler,
+}) => {
+  return (
+    <div className="profile">
+      <section className="profile-sidebar">
+        <SideBar name={nameOfPerson} />
+      </section>
+      <section className="profile-clothes">
+        <ClothesSection
+          clothingItemArray={cards}
+          onCardClick={onCardClick}
+          onCardDelete={onCardDelete}
+          onAddNewClick={onAddNewClick}
+          clothingType={clothingType}
+          clickHandler={clickHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Profile;
